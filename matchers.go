b9ef51935b4fe519a3870c706c0fb28c0f5d7803
NekoFluff <alexnou@gmail.com>
2024-10-29 16:03:45 -0700

    feat: add ContainsInteractionResponseMatcher for testing purposes

---
+++ matchers.go
@@ -0,0 +1,34 @@
+package discord
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type ContainsInteractionResponseMatcher struct {
+	msg string
+}
+
+func (m ContainsInteractionResponseMatcher) Matches(input interface{}) bool {
+	response, ok := input.(*discordgo.InteractionResponse)
+	if !ok {
+		return false
+	}
+
+	return strings.Contains(response.Data.Content, m.msg)
+}
+
+func (m ContainsInteractionResponseMatcher) String() string {
+	return fmt.Sprintf("to contain msg '%v'", m.msg)
+}
+
+func (m ContainsInteractionResponseMatcher) Got(input interface{}) string {
+	response, ok := input.(*discordgo.InteractionResponse)
+	if !ok {
+		return "not an interaction response"
+	}
+
+	return response.Data.Content
+}
